842a6b64b802d661ad3996f65531bd8c9bba5e35
Joel Andrade <joelmanrique38@gmail.com>
2025-02-19 07:18:26 -0600

    ssegunda actualizacion camping

--- js/main.js
+++ js/main.js
@@ -4,3 +4,37 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () =>{
     menu.classList.toggle("move")
 }
+
+//categories swipper 
+var swiper = new Swiper(".categorySwiper", {
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        280:{
+            slidesPerView:1,
+            spaceBetween:10,
+        },
+        510:{
+            slidesPerView:2,
+            spaceBetween:10,
+        },
+        750:{
+            slidesPerView:3,
+            spaceBetween:15,
+        },
+        900:{
+            slidesPerView:4,
+            spaceBetween:20,
+        }
+    }
+  });
